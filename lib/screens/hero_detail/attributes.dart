7a2b641cf7388432e1088c1e5a9a61e7ee0be979
Tyler <tylerpham.9@gmail.com>
2019-05-24 17:52:03 -0700

    separate attributes details

---
+++ lib/screens/hero_detail/attributes.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AttributeDetail extends StatelessWidget {
+  final String _attr;
+  final int _attrBase;
+  final double _attrGain;
+  final String _primaryAttr;
+  final colorMap = {'str': Colors.red, 'agi': Colors.green, 'int': Colors.blue};
+  final iconMap = {
+    'str': "assets/icons/strIcon.png",
+    'agi': "assets/icons/agiIcon.png",
+    'int': "assets/icons/intIcon.png",
+  };
+
+  AttributeDetail(
+      this._attr, this._attrBase, this._attrGain, this._primaryAttr);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Container(
+                width: 45.0,
+                height: 45.0,
+                child: DecoratedBox(decoration: getAttrIcon()))),
+        Row(
+          children: <Widget>[
+            Container(
+                child: Text(_attrBase.toString(),
+                    style: TextStyle(color: colorMap[_attr]))),
+            Container(child: Text(' + ' + _attrGain.toString())),
+          ],
+        )
+      ],
+    );
+  }
+
+  BoxDecoration getAttrIcon() {
+    return BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(iconMap[_attr]), fit: BoxFit.cover),
+        borderRadius: BorderRadius.all(Radius.circular(45.0)),
+        border: new Border.all(
+            color: (_attr == _primaryAttr) ? Colors.amber : Colors.black,
+            width: 2.0));
+  }
+}
--- lib/screens/hero_detail/hero_detail.dart
+++ lib/screens/hero_detail/hero_detail.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+
+//Models
 import 'package:dota2app/models/Hero.dart';
+
+//Subsections
 import 'package:dota2app/screens/hero_detail/stats_table.dart';
 import 'package:dota2app/screens/hero_detail/additional_details_table.dart';
+import 'package:dota2app/screens/hero_detail/attributes.dart';
+
+//Style
 import 'package:dota2app/style.dart';
 
 class HeroDetail extends StatelessWidget {
@@ -31,7 +38,7 @@ class HeroDetail extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(mainAxisAlignment: MainAxisAlignment.start,
-//        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Center(
                 child: Container(
@@ -88,54 +95,5 @@ class HeroDetail extends StatelessWidget {
         ),
       )
     ]);
-  }
-}
-
-class AttributeDetail extends StatelessWidget {
-  final String _attr;
-  final int _attrBase;
-  final double _attrGain;
-  final String _primaryAttr;
-  final colorMap = {'str': Colors.red, 'agi': Colors.green, 'int': Colors.blue};
-  final iconMap = {
-    'str': "assets/icons/strIcon.png",
-    'agi': "assets/icons/agiIcon.png",
-    'int': "assets/icons/intIcon.png",
-  };
-
-  AttributeDetail(
-      this._attr, this._attrBase, this._attrGain, this._primaryAttr);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Container(
-                width: 45.0,
-                height: 45.0,
-                child: DecoratedBox(decoration: getAttrIcon()))),
-        Row(
-          children: <Widget>[
-            Container(
-                child: Text(_attrBase.toString(),
-                    style: TextStyle(color: colorMap[_attr]))),
-            Container(child: Text(' + ' + _attrGain.toString())),
-          ],
-        )
-      ],
-    );
-  }
-
-  BoxDecoration getAttrIcon() {
-    return BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage(iconMap[_attr]), fit: BoxFit.cover),
-        borderRadius: BorderRadius.all(Radius.circular(45.0)),
-        border: new Border.all(
-            color: (_attr == _primaryAttr) ? Colors.amber : Colors.black,
-            width: 2.0));
   }
 }
